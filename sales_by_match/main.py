f3fc23ab27ba411af4dcd68534c10f6d9b2ddb40
Emanuel <em.dementei@gmail.com>
2021-11-25 21:22:30 +0100

    Sales by Match solved
    https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true
    
    solved

---
+++ sales_by_match/main.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+
+    pila = []
+    sumatorio = 0
+    for i in ar:
+        if i in pila:
+            sumatorio += 1
+            pila.remove(i)
+        else:
+            pila.append(i)
+
+    return sumatorio
+
+
+if __name__ == '__main__':
+    fptr = open("output.txt", 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
